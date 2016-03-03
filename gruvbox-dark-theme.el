a0f858ee46abe3cdeddf3caae7fce3d0fe34e064
Florian Besser <fbesser@gmail.com>
2016-03-03 02:04:55 +0100

    Fix ivy-mode color names

--- gruvbox-dark-theme.el
+++ gruvbox-dark-theme.el
@@ -628,13 +628,13 @@
    `(outline-8 ((t (:foreground ,neutral_orange :weight bold))))
 
    ;; ivy
-   `(ivy-current-match           ((t (:weight bold :background ,gruvbox-neutral_orange :foreground ,gruvbox-dark0))))
-   `(ivy-minibuffer-match-face-1 ((t (:weight bold :background ,gruvbox-bright_orange :foreground ,gruvbox-dark0))))
-   `(ivy-minibuffer-match-face-2 ((t (:weight bold :background ,gruvbox-bright_purple :foreground ,gruvbox-dark0))))
-   `(ivy-minibuffer-match-face-3 ((t (:weight bold :background ,gruvbox-bright_aqua :foreground ,gruvbox-dark0))))
-   `(ivy-minibuffer-match-face-4 ((t (:weight bold :background ,gruvbox-bright_blue :foreground ,gruvbox-dark0))))
-   `(ivy-remote                  ((t (:foreground ,gruvbox-neutral_purple))))
-   `(ivy-modified-buffer         ((t (:foreground ,gruvbox-neutral_red))))
+   `(ivy-current-match           ((t (:weight bold :background ,neutral_orange :foreground ,dark0))))
+   `(ivy-minibuffer-match-face-1 ((t (:weight bold :background ,bright_orange :foreground ,dark0))))
+   `(ivy-minibuffer-match-face-2 ((t (:weight bold :background ,bright_purple :foreground ,dark0))))
+   `(ivy-minibuffer-match-face-3 ((t (:weight bold :background ,bright_aqua :foreground ,dark0))))
+   `(ivy-minibuffer-match-face-4 ((t (:weight bold :background ,bright_blue :foreground ,dark0))))
+   `(ivy-remote                  ((t (:foreground ,neutral_purple))))
+   `(ivy-modified-buffer         ((t (:foreground ,neutral_red))))
 
    ;; haskell-mode
    '(haskell-debug-muted-face        ((t (:inherit shadow))))
